143a70d933fda25966852927a5c539344a6e2778
Michael Bonfils <michael.bonfils@sinch.com>
2023-03-06 19:13:21 +0100

    Fix cast

--- go/src/main.go
+++ go/src/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/aquilax/go-perlin"
 )
 
@@ -13,14 +11,6 @@ import (
 var instance *perlin.Perlin
 
 func main() {
-	p := perlin.NewPerlin(2, 2, 1, 42)
-
-	for x := 0; x <= 1000; x++ {
-		for y := 0; y <= 1000; y++ {
-			_ = p.Noise2D(float64(x)/100, float64(y)/100)
-		}
-	}
-	fmt.Println("End...")
 }
 
 /* DON'T ADD SPACE BEFORE export ! */
@@ -36,6 +26,7 @@ func NewPerlin(seed int) int {
 }
 
 //export Perlin
-func Perlin(x, y int) int {
-	return int(instance.Noise2D(float64(x), float64(y)))
+func Perlin(x, y float32) float32 {
+	r := instance.Noise2D(float64(x), float64(y))
+	return float32(r)
 }
