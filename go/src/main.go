dcfbbcc5a945075e6fea434ce16aa3dabdd002b0
Michael Bonfils <michael.bonfils@sinch.com>
2023-03-08 19:45:45 +0100

    Add measure from GO

--- go/src/main.go
+++ go/src/main.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"github.com/aquilax/go-perlin"
+    "fmt"
+    "time"
 )
-
-// "fmt"
-
-// perlin "github.com/aquilax/go-perlin"
 
 var instance *perlin.Perlin
 
 func main() {
 }
+
+func main_test() {
+	start := time.Now()
+
+	p := perlin.NewPerlin(2, 2, 1, 42)
+	for x := 0; x <= 5000; x++ {
+		for y := 0; y <= 5000; y++ {
+			_ = p.Noise2D(float64(x)/100, float64(y)/100)
+			_ = p.Noise2D(float64(x)/100, float64(y)/100)
+		}
+	}
+	t := time.Now()
+	fmt.Println("It tooks", t.Sub(start), "ms")
+}
+
 
 /* DON'T ADD SPACE BEFORE export ! */
 //export multiply
